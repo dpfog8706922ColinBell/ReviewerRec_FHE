8c4ff43f69e620319f1d03e2ddb158f736385b43
dpfog8706922ColinBell <SuresBrodhead9901@outlook.com>
2025-12-04 22:37:01 +0800

    add global.jsx, import-fragments.py, UniversalAdapter.ts

---
+++ frontend/web/src/api/fragments/responsive-service/import-fragments.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\api\fragments\responsive-service\import-fragments.py
+// Generated at: 2025-10-16T06:41:25.406Z
---
+++ global.jsx
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+interface mutations-boilerplateProps { data: string }
